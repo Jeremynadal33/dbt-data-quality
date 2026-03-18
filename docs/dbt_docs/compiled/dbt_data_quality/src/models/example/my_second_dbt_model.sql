ea74d0d5c0b99db15847aca43e1b32213d8fec72
Jeremy Nadal <jjjnadal33@gmail.com>
2026-03-18 16:43:19 +0100

    feat: init gh-pages

---
+++ docs/dbt_docs/compiled/dbt_data_quality/src/models/example/my_second_dbt_model.sql
@@ -0,0 +1,5 @@
+-- Use the `ref` function to select from other models
+
+select *
+from jnadal_db.jnadal_db.my_first_dbt_model
+where id = 1
---
+++ docs/dbt_docs/compiled/dbt_data_quality/src/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from jnadal_db.jnadal_db.my_second_dbt_model
+where id is null
+
+
